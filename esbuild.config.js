80ec84513bdf50efca68b59157d3790b5e600a3b
Fabian Siegel <fabians1999@gmail.com>
2021-10-03 19:13:24 +0200

    esbuild-plugin-html: read html content from public/ directory

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,59 +1,56 @@
 const esbuild = require('esbuild')
-
-const watch = process.argv.includes('--watch')
-const development = process.argv.includes('--development')
-const serve = process.argv.includes('--serve')
-
+const fs = require('fs/promises')
 const { htmlPlugin } = require('@craftamap/esbuild-plugin-html')
 
-const options = {
-  entryPoints: ['src/index.jsx', 'src/auth-flow-response.js'],
-  bundle: true,
-  outdir: 'dist/',
-  watch: watch,
-  minify: !development,
-  sourcemap: development,
-  entryNames: '[dir]/[name]-[hash]',
-  logLevel: 'debug',
-  metafile: true,
-  plugins: [
-    htmlPlugin({
-      files: [
-        {
-          entryPoints: [
-            'src/index.jsx',
-          ],
-          filename: 'index.html',
-          htmlTemplate: `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            </head>
-            <body>
-                <div id="root">
-                </div>
-            </body>
-            </html>
-          `,
-        },
-        {
-          entryPoints: [
-            'src/auth-flow-response.js',
-          ],
-          filename: 'auth-flow-response.html',
-        },
-      ],
-    }),
-  ],
+const exec = async () => {
+  const watch = process.argv.includes('--watch')
+  const development = process.argv.includes('--development')
+  const serve = process.argv.includes('--serve')
+
+  const indexContent = await fs.readFile('./public/index.html')
+  const authContent = await fs.readFile('./public/auth-flow-response.html')
+
+  const options = {
+    entryPoints: ['src/index.jsx', 'src/auth-flow-response.js'],
+    bundle: true,
+    outdir: 'dist/',
+    watch: watch,
+    minify: !development,
+    sourcemap: development,
+    entryNames: '[dir]/[name]-[hash]',
+    logLevel: 'debug',
+    metafile: true,
+    plugins: [
+      htmlPlugin({
+        files: [
+          {
+            entryPoints: [
+              'src/index.jsx',
+            ],
+            filename: 'index.html',
+            htmlTemplate: indexContent,
+          },
+          {
+            entryPoints: [
+              'src/auth-flow-response.js',
+            ],
+            filename: 'auth-flow-response.html',
+            htmlTemplate: authContent
+          },
+        ],
+      }),
+    ],
+  }
+  if (serve) {
+    await esbuild.serve({
+      servedir: 'dist',
+      port: 8000,
+    }, options)
+  } else {
+    await esbuild.build(options)
+      .catch(() => process.exit(1))
+  }
 }
 
-if (serve) {
-  esbuild.serve({
-    servedir: 'dist',
-    port: 8000,
-  }, options)
-} else {
-  esbuild.build(options).catch(() => process.exit(1))
-}
+
+exec()
